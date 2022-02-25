8d69f48e380ac9875cf8abd3b27d676dd1c96cc9
jc <jc@houseofagile.com>
2022-02-25 11:48:01 +0100

    add doctrine enum types

---
+++ src/DBAL/Types/NakaMenuItemType.php
@@ -0,0 +1,22 @@
+<?php
+namespace HouseOfAgile\NakaCMSBundle\DBAL\Types;
+
+use Fresh\DoctrineEnumBundle\DBAL\Types\AbstractEnumType;
+
+final class NakaMenuItemType extends AbstractEnumType
+{
+    public const TYPE_ROUTE = 'route';
+    public const TYPE_URL = 'url';
+    public const TYPE_PAGE = 'page';
+
+    protected static $choices = [
+        self::TYPE_ROUTE => 'Menu Item of type route',
+        self::TYPE_URL => 'Menu Item of type url',
+        self::TYPE_PAGE => 'Menu Item of type page',
+    ];
+
+    public static function getGuessOptions()
+    {
+        return array_flip(self::$choices);
+    }
+}
