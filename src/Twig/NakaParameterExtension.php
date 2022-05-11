20492c0ea7a126e1b2ba5f146ec152c79c079213
jc <jc@houseofagile.com>
2022-05-11 18:54:10 +0200

    Add google-analytics parameter and fragment

---
+++ src/Twig/NakaParameterExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HouseOfAgile\NakaCMSBundle\Twig;
+
+use HouseOfAgile\NakaCMSBundle\Component\NakaParameter\NakaParameterManager;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class NakaParameterExtension extends AbstractExtension
+{
+    /** @var NakaParameterManager */
+    protected $nakaParameterManager;
+
+    protected $allNakaParameter;
+
+    public function __construct(NakaParameterManager $nakaParameterManager)
+    {
+        $this->nakaParameterManager = $nakaParameterManager;
+        $this->allNakaParameter = $nakaParameterManager->getAllParameter();
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('nakaParameter', [$this, 'nakaParameter']),
+        ];
+    }
+
+    public function nakaParameter($parameterKey)
+    {
+        $nakaParameter = $this->nakaParameterManager->getNakaParameter($parameterKey);
+        if ($nakaParameter) {
+            return $nakaParameter->getValue();
+        } else {
+            // dd($this->allNakaParameter);
+        }
+    }
+}
