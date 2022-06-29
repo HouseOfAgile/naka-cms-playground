329f88a8d05de3d005fde724918aefb2d1b9bc80
jc <jc@houseofagile.com>
2022-06-29 14:42:12 +0200

    Update some translation and the contact page

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,8 +4,10 @@ namespace HouseOfAgile\NakaCMSBundle\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +16,13 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('subject')
-            ->add('message', TextareaType::class, ['attr' => array('rows' => '6')])
+            ->add('name', TextType::class, ['label' => 'form.contact.name'])
+            ->add('email', EmailType::class, ['label' => 'form.contact.email'])
+            ->add('subject', TextType::class, ['label' => 'form.contact.subject'])
+            ->add('message', TextareaType::class, [
+                'label' => 'form.contact.message',
+                'attr' => array('rows' => '6')
+            ])
             ->add('submit', SubmitType::class, ['label' => 'form.contact.submit']);
     }
 
