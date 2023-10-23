3bc2c84b7ea51657b4249eeed5a9280afd158099
jc <jc@houseofagile.com>
2023-10-23 18:26:13 +0200

    Refactor User info form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,69 +2,21 @@
 
 namespace HouseOfAgile\NakaCMSBundle\Form;
 
-use App\Entity\User;
-use HouseOfAgile\NakaCMSBundle\Form\Type\AddressType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationFormType extends AbstractType
+class RegistrationFormType extends UserInfoType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        parent::buildForm($builder, $options);
         $builder
-            ->add('firstName', TextType::class, [
-                'label' => 'form.member.firstName',
-                'attr' => [
-                    'placeholder' => 'form.member.firstName',
-                ],
-                'row_attr' => [
-                    'class' => 'form-floating',
-                ],
-            ])
-            ->add('lastName', TextType::class, [
-                'label' => 'form.member.lastName',
-                'attr' => [
-                    'placeholder' => 'form.member.lastName',
-                ],
-                'row_attr' => [
-                    'class' => 'form-floating',
-                ],
-            ])
-            ->add('birthDate', BirthdayType::class, [
-                'format' => $options['locale'] == 'de' ? 'd.M.y' : 'd/M/y',
-                'label' => 'form.member.birthDate',
-                'help' => 'form.member.birthDate.help',
-                'required' => false,
-                'widget' => 'single_text',
-                'html5' => false,
-                'attr' => ['class' => 'p-3 js-datepicker text-center'],
-            ])
-            ->add('address', AddressType::class, [
-                'label' => 'form.member.address',
-                'help' => 'form.member.address.help',
-                'data_class' => User::class,
-                'required' => false,
-            ])
-            ->add('email', EmailType::class, [
-                'label' => 'form.member.email',
-                'attr' => [
-                    'placeholder' => 'form.member.email',
-                ],
-                'row_attr' => [
-                    'class' => 'form-floating',
-                ],
-            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'label' => 'form.member.agreeTerms',
                 'label_attr' => ['class' => 'py-2'],
@@ -118,13 +70,5 @@ class RegistrationFormType extends AbstractType
             ->add('register', SubmitType::class, [
                 'label' => 'form.member.register',
             ]);
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-            'locale' => 'en'
-        ]);
     }
 }
---
+++ src/Form/UserInfoType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace HouseOfAgile\NakaCMSBundle\Form;
+
+use App\Entity\User;
+use HouseOfAgile\NakaCMSBundle\Form\Type\AddressType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserInfoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'form.member.firstName',
+                'attr' => [
+                    'placeholder' => 'form.member.firstName',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'form.member.lastName',
+                'attr' => [
+                    'placeholder' => 'form.member.lastName',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('birthDate', BirthdayType::class, [
+                'label' => 'form.member.birthDate',
+                'help' => 'form.member.birthDate.help',
+                'required' => false,
+                'format' => $options['locale'] == 'de' ? 'd.M.y' : 'd/M/y',
+                'widget' => 'single_text',
+                'html5' => false,
+                'attr' => ['class' => 'p-3 js-datepicker text-center'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'form.member.email',
+                'attr' => [
+                    'placeholder' => 'form.member.email',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
+            ->add('address', AddressType::class, [
+                'label' => 'form.member.address',
+                'help' => 'form.member.address.help',
+                'data_class' => User::class,
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'locale' => 'en'
+        ]);
+    }
+}
