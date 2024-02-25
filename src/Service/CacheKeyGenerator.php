e50e326f8dec20775f817a11eef307f41fc33db3
jc <jc@houseofagile.com>
2024-02-25 14:49:25 +0100

    Add common cacheKeyGenerator Service

---
+++ src/Service/CacheKeyGenerator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HouseOfAgile\NakaCMSBundle\Service;
+
+class CacheKeyGenerator
+{
+    /**
+     * Generates a cache key with an optional random ID component, keeping the base string info.
+     *
+     * @param string $baseString The base string for the cache key.
+     * @param array $identifiers An array of identifiers to make the cache key specific.
+     * @param bool $includeRandomId Whether to include a random ID component in the cache key.
+     * @return string The generated cache key.
+     */
+    public function generate(string $baseString, array $identifiers = [], bool $includeRandomId = true): string
+    {
+        $keyComponents = [$baseString];
+        if (!empty($identifiers)) {
+            $keyComponents = array_merge($keyComponents, $identifiers);
+        }
+        $cacheKey = implode('_', array_map('strval', $keyComponents));
+
+        if ($includeRandomId) {
+            $randomId = bin2hex(random_bytes(8));
+            $cacheKey .= "_rand_{$randomId}";
+        }
+
+        return $cacheKey;
+    }
+}
