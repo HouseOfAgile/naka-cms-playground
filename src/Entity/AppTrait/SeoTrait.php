08b2b17ffcb873a951ff243e0b8b4f1f21243d4e
jc <jc@houseofagile.com>
2024-05-15 12:39:22 +0000

    Add SeoTrait to manage basic SEO attributes on selected entities

---
+++ src/Entity/AppTrait/SeoTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace HouseOfAgile\NakaCMSBundle\Entity\AppTrait;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait SeoTrait
+{
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $metaTitle = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $metaDescription = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $metaKeywords = null;
+
+    public function getMetaTitle(): ?string
+    {
+        return $this->metaTitle;
+    }
+
+    public function setMetaTitle(?string $metaTitle): self
+    {
+        $this->metaTitle = $metaTitle;
+        return $this;
+    }
+
+    public function getMetaDescription(): ?string
+    {
+        return $this->metaDescription;
+    }
+
+    public function setMetaDescription(?string $metaDescription): self
+    {
+        $this->metaDescription = $metaDescription;
+        return $this;
+    }
+
+    public function getMetaKeywords(): ?string
+    {
+        return $this->metaKeywords;
+    }
+
+    public function setMetaKeywords(?string $metaKeywords): self
+    {
+        $this->metaKeywords = $metaKeywords;
+        return $this;
+    }
+}
