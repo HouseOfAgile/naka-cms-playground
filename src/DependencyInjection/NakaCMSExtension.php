30468d0cd2dc7ce08e4785c180087f0b3225208c
jc <jc@houseofagile.com>
2024-08-01 21:48:30 +0000

    Update conf and default values

--- src/DependencyInjection/NakaCMSExtension.php
+++ src/DependencyInjection/NakaCMSExtension.php
@@ -20,12 +20,12 @@ class NakaCMSExtension extends Extension implements PrependExtensionInterface
 
         $definition = $container->getDefinition('hoa_naka_cms.hoa_ipsum');
         $definition->setArgument(0, $config['internationalization']['all_locales']);
-        $definition->setArgument(1, $config['internationalization']['supported_locales']);
+        $definition->setArgument(1, explode('|', $config['internationalization']['supported_locales']));
 
         $container->setParameter('hoa_naka_cms.redirect_url', $config['redirect_url']);
-        $container->setParameter('hoa_naka_cms.openai_prompts', $config['openai_prompts']);
-
-
+        $container->setParameter('hoa_naka_cms.openai_prompts.default_word_count', $config['openai_prompts']['default_word_count']);
+        $container->setParameter('hoa_naka_cms.openai_prompts.additional_instructions', $config['openai_prompts']['additional_instructions']);
+        $container->setParameter('hoa_naka_cms.openai_prompts.prompts', $config['openai_prompts']['prompts']);
     }
 
     public function prepend(ContainerBuilder $container): void
@@ -43,5 +43,10 @@ class NakaCMSExtension extends Extension implements PrependExtensionInterface
     public function getAlias(): string
     {
         return 'hoa_naka_cms';
+    }
+
+    public function getConfiguration(array $config, ContainerBuilder $container)
+    {
+        return new Configuration();
     }
 }
